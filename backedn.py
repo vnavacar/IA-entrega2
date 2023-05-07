e45a93e0f41ec7a485d1ff8f8a4bf44597053b98
Gabriel Pelegri <gpelerii@myuax.com>
2023-05-07 19:06:30 +0200

    si

---
+++ backedn.py
@@ -0,0 +1,33 @@
+from flask import Flask, request, jsonify
+import pickle
+import numpy as np
+from sklearn.feature_extraction.text import CountVectorizer, TfidfVectorizer
+
+app = Flask(__name__,template_folder='templates')
+
+# Load the classifier
+with open("Python/classifier.pkl", "rb") as f:
+    classifier = pickle.load(f)
+
+@app.route("/", methods=["POST"])
+def predict():
+    text = request.json["text"]
+    # Preprocess the input text using the same method you used to train the classifier
+    processed_text = preprocess(text)
+    prediction = classifier.predict(np.array([processed_text]))[0]
+     # Convert the prediction to a genre 
+    return jsonify({"response_text": prediction})
+
+if __name__ == "__index__":
+    app.run(debug=True)
+
+def preprocess(text):
+    # Preprocess the input text
+    text = text.lower()
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("\r", " ")
+    vectorizador = CountVectorizer(stop_words="english")
+    vectorizador.fit([text])
+
+    return text
