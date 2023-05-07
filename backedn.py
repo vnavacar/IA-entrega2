12220a8579244d1b06d20f1487347b18540a368f
Gabriel Pelegri <gpelerii@myuax.com>
2023-05-07 19:21:57 +0200

    cambios

--- backedn.py
+++ backedn.py
@@ -9,16 +9,17 @@ app = Flask(__name__,template_folder='templates')
 with open("Python/classifier.pkl", "rb") as f:
     classifier = pickle.load(f)
 
+
 @app.route("/", methods=["POST"])
 def predict():
     text = request.json["text"]
     # Preprocess the input text using the same method you used to train the classifier
-    processed_text = preprocess(text)
+    processed_text = preprocess(text)   
     prediction = classifier.predict(np.array([processed_text]))[0]
      # Convert the prediction to a genre 
     return jsonify({"response_text": prediction})
 
-if __name__ == "__index__":
+if __name__ == "__main__":
     app.run(debug=True)
 
 def preprocess(text):
@@ -31,3 +32,32 @@ def preprocess(text):
     vectorizador.fit([text])
 
     return text
+with open("Python/classifier.pkl", "rb") as f:
+    classifier = pickle.load(f)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def main():
+    if app.request.method == 'GET':
+        return(app.render_template('main.html'))
+    if app.request.method == 'POST':
+        text = request.json["text"]
+    # Preprocess the input text using the same method you used to train the classifier
+        processed_text = preprocess(text)   
+        prediction = classifier.predict(np.array([processed_text]))[0]
+     # Convert the prediction to a genre 
+        return jsonify({"response_text": prediction})
+    
+def preprocess(text):
+    # Preprocess the input text
+    text = text.lower()
+    text = text.replace("\n", " ")
+    text = text.replace("\t", " ")
+    text = text.replace("\r", " ")
+    vectorizador = CountVectorizer(stop_words="english")
+    vectorizador.fit([text])
+
+    return text    
+if __name__ == "__main__":
+    app.run(debug=True)
+                                 
