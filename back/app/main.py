43520cb911a7fd78de31a96e41e6a1b647ef325a
Gabriel Pelegri <gpelerii@myuax.com>
2023-05-08 18:16:51 +0200

    si

--- back/app/main.py
+++ back/app/main.py
@@ -6,20 +6,24 @@ from flask_cors import CORS
 app= Flask(__name__)
 CORS(app)
 
-with open("classifier.pkl", "rb") as f:
-    classifier = pickle.load(f)
+#with open("classifier.pkl", "rb") as f:
+ #   classifier = pickle.load(f)
+
+
 
 @app.route('/', methods=['POST'])
 def prediction():
-    text = request.json["text"]
+    classifier = pickle.load(open('classifier.pkl','rb'))
+    text = request.get_json("text")
     # Preprocess the input text using the same method you used to train the classifier
     #processed_text = preprocess(text)
-    prediction = classifier.predict(text["text"])
+    prediction = classifier.predict([text["text"]])
+    
      # Convert the prediction to a genre 
-    if prediction == "Comedia":
-        response= jsonify({"Categoria": "Comedia"})
-    elif prediction == "Horror":
-        response= jsonify({"Categoria": "Horror"})
-    return response
+    if prediction == 0:
+        return jsonify({"categoria": "Comedia"})
+    if prediction == 1:
+        return jsonify({"categoria": "Horror"})
+    
 
 	
